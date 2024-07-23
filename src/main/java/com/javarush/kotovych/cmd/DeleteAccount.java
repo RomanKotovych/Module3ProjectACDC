ef46ed6953e9f92aea6d16671ceb2576f57cf128
RomanKotovych <kotovich.roman2010@gmail.com>
2024-07-23 17:05:18 -0500

    FIX: exception when deleting user

--- src/main/java/com/javarush/kotovych/cmd/DeleteAccount.java
+++ src/main/java/com/javarush/kotovych/cmd/DeleteAccount.java
@@ -3,6 +3,7 @@ package com.javarush.kotovych.cmd;
 import com.javarush.kotovych.constants.Constants;
 import com.javarush.kotovych.constants.LoggerConstants;
 import com.javarush.kotovych.constants.UriConstants;
+import com.javarush.kotovych.dto.UserTo;
 import com.javarush.kotovych.service.UserService;
 import com.javarush.kotovych.util.SessionAttributeSetter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,11 +21,11 @@ public class DeleteAccount {
     private final UserService userService;
 
     @PostMapping(UriConstants.DELETE_ACCOUNT_URI)
-    public ModelAndView deleteAccount(@SessionAttribute(value = Constants.ID) Long id,
+    public ModelAndView deleteAccount(@SessionAttribute(value = Constants.USER) UserTo user,
                                       HttpServletRequest request) {
-        userService.deleteById(id);
+        userService.deleteById(user.getId());
         SessionAttributeSetter.addSessionAttribute(request, Constants.ID, String.valueOf(Constants.DEFAULT_ID));
-        log.info(LoggerConstants.USER_ID_DELETED_LOG, id);
+        log.info(LoggerConstants.USER_ID_DELETED_LOG, user.getId());
         return new ModelAndView(Constants.MAIN_PAGE_REDIRECT);
     }
 }
